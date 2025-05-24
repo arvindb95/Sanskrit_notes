43b6bae36603e98a73e709fe75ef9179de7d20cd
Arvind Balasubramanian <31187160+arvindb95@users.noreply.github.com>
2025-05-24 19:11:34 +0530

    Delete xournal_files directory

--- xournal_files/export_annotated_pdf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-flatpak run com.github.xournalpp.xournalpp learnsanskrit_online_guide_Arvind_solutions.xopp -p temp.pdf --export-no-background
-qpdf learnsanskrit_online_guide.pdf --overlay temp.pdf -- learnsanskrit_online_guide_annotated.pdf
-rm temp.pdf
